de30ef89e9754e850a6a8f3d7aeaa1a2b1e4d3ca
PatrikRasch <patrikrask@hotmail.com>
2023-06-23 17:43:22 +0200

    Context for cart wraps rest of app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // import "./App.css";
+import RouteSwitch from "./components/RouteSwitch";
+import "./app.css";
+import { CartProvider } from "./components/CartContext";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Initial</h1>
-    </div>
+    <CartProvider>
+      <RouteSwitch />
+    </CartProvider>
   );
 }
 
