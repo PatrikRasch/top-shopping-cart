20c18a3818659b3260d824edd6bdc15b0a1283ee
PatrikRasch <patrikrask@hotmail.com>
2023-06-25 15:01:10 +0200

    Added orange styling and check symbol when add to cart is clicked

--- src/components/productsPage/product.js
+++ src/components/productsPage/product.js
@@ -2,10 +2,12 @@ import "./product.css";
 import { useCart } from "../CartContext";
 import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
+import checkSymbol from "./../../images/check-symbol.svg";
 
 const Product = (props) => {
   const { addItemToCart, addToCartTotal } = useCart();
   const [addToCartClicked, setAddToCartClicked] = useState(false);
+  const [itemAddedToCart, setItemAddedToCart] = useState(false);
 
   const handleAddToCartClicked = () => {
     setAddToCartClicked(true);
@@ -29,6 +31,13 @@ const Product = (props) => {
     addToCartTotal();
   };
 
+  const changeAddToCartTextOnClick = () => {
+    setItemAddedToCart(true);
+    return setTimeout(() => {
+      setItemAddedToCart(false);
+    }, 750);
+  };
+
   return (
     <div className="all-products-wrapper">
       <div className="product-card">
@@ -39,13 +48,16 @@ const Product = (props) => {
           <button
             className={`product-add-to-cart-button ${
               addToCartClicked ? "product-add-to-cart-button-clicked-button" : ""
-            }`}
+            } ${itemAddedToCart ? "item-added-to-cart" : ""}`}
             onClick={() => {
               passItem();
               handleAddToCartClicked();
+              changeAddToCartTextOnClick();
             }}
           >
-            <div className="product-add-to-cart-text">Add to cart</div>
+            <div className="product-add-to-cart-text">
+              {itemAddedToCart ? <img src={checkSymbol} alt="Added to cart" /> : "Add to cart"}
+            </div>
           </button>
         </div>
       </div>
